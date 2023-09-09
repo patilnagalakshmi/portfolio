5c229d658ee616cd08c6e92c2497da4485024d91
patilnagalakshmi <140230625+patilnagalakshmi@users.noreply.github.com>
2023-09-09 23:37:38 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+let type= new Typed('#changing-text',{
+    strings:['Electronics Engineer','upcoming developer','Frontend Developer','Software Developer'],
+    typeSpeed:50,
+    backSpeed:50,
+    loop:true,
+})
